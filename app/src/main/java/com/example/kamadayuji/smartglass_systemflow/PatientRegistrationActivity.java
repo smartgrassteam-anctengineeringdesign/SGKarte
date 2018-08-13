8bf8674d5acbdbbb2aa236de6af4a900e4636f0b
鎌田悠司 <fyuken322@gmail.com>
2018-08-14 01:56:29 +0900

    DBアダプターの作成及び、性別の追加

--- app/src/main/java/com/example/kamadayuji/smartglass_systemflow/PatientRegistrationActivity.java
+++ app/src/main/java/com/example/kamadayuji/smartglass_systemflow/PatientRegistrationActivity.java
@@ -2,8 +2,20 @@ package com.example.kamadayuji.smartglass_systemflow;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PatientRegistrationActivity extends AppCompatActivity {
+
+    private EditText mEditText04Name;           // 氏名
+    private EditText mEditText04Age;            // 年齢
+    private EditText mEditText04Affiliation;    // 所属
+    private EditText mEditText04Detail;         // 詳細
+
+    private TextView mText04Kome01;             // 氏名の※印
+    private TextView mText04Kome02;             // 年齢の※印
+    private TextView mText04Kome03;             // 個数の※印
+    private TextView mText04Kome04;             // 単価の※印
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
