ffd83c19e878ce55dba9cc985abdd7e31af081a4
Hanahuda <p188014@edu.asahikawa-nct.ac.jp>
2019-09-05 10:45:08 +0900

    Android 正常実行官僚

--- openCVLibrary343/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary343/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/kamadayuji/AndroidStudioProjects/smartGlass_systemflow/openCVLibrary343/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\hana\\Documents\\SGKarte\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
